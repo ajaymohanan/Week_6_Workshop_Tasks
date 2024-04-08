9f7cd1797271d99a92a8a66a831c7200b6787031
muizz <muizz@DESKTOP-ASR9N5V>
2024-04-08 16:10:13 +0800

    Task 1 Fixed

--- Task_1.c
+++ Task_1.c
@@ -41,11 +41,11 @@ void filter_ascending(char* string_1)
 {
     for (int i = 1; i < strlen(string_1); i++)
     {
-        for (int j = 0; string_1[j+2] != '\0'; j++)
+        for (int j = 0; string_1[j+1] != '\0'; j++)//
         {
             if (string_1[j] > string_1[j+1])
             {
-                for (int k = 0; string_1[k] != '\0'; k++)
+                for (int k = j+1; string_1[k] != '\0'; k++)//
                 {
                     string_1[k] = string_1[k+1];
                 }
